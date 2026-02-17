00306f8a8df90b86fd67cd5d99e2dba65ea0b7b7
Mohamed Moughamir <moughamir@gmail.com>
2026-02-17 17:56:39 +0000

    refactor: Set default theme to light, remove the navigation bar, and delete the welcome heading.

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-between">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-          <div className="flex gap-5 items-center font-semibold">
-            <Link href={"/"}>anaqio</Link>
-          </div>
-          <div className="flex items-center gap-4">
-            <Suspense>
-              <AuthButton />
-            </Suspense>
-            <ThemeSwitcher />
-          </div>
-        </div>
-      </nav>
+      {
+        /**
+               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <div className="flex gap-5 items-center font-semibold">
+                  <Link href={"/"}>anaqio</Link>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Suspense>
+                    <AuthButton />
+                  </Suspense>
+                  <ThemeSwitcher />
+                </div>
+              </div>
+            </nav>
+         */
+      }
 
       <div className="flex-1 flex items-center justify-center w-full">
         <div className="flex flex-col items-center gap-8">
@@ -31,7 +35,6 @@ export default function Home() {
             priority
             className="w-auto h-auto"
           />
-          <h1 className="text-4xl font-bold text-center">Welcome to anaqio</h1>
         </div>
       </div>
 
