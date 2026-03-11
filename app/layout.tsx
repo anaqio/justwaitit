c9f8b9f21e36298bc87f6bcc7faaf0df7213a484
Mohamed Moughamir <moughamir@gmail.com>
2026-03-11 01:27:13 +0000

    feat: Add Vercel Analytics and Speed Insights, and create `resume.txt`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import type { Metadata, Viewport } from 'next';
 
@@ -99,6 +101,8 @@ export default function RootLayout({
         {children}
         <GrainOverlay />
       </body>
+      <Analytics />
+      <SpeedInsights />
       {process.env.NEXT_PUBLIC_GA_ID && (
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
       )}
